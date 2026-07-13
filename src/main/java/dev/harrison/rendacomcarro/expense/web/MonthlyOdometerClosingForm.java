04fde8bebda46b588ab58870be5acbc667b24de1
Harrison Marques Freitas <57815260+harrison-m-freitas@users.noreply.github.com>
2026-07-13 09:43:19 -0300

    feat: support inferred mileage confirmation form

--- src/main/java/dev/harrison/rendacomcarro/expense/web/MonthlyOdometerClosingForm.java
+++ src/main/java/dev/harrison/rendacomcarro/expense/web/MonthlyOdometerClosingForm.java
@@ -27,53 +27,26 @@ public class MonthlyOdometerClosingForm {
     @DecimalMin("0.0")
     private BigDecimal professionalKilometers;
 
+    private boolean manualAdjustment;
+    private boolean confirmWarnings;
     private String adjustmentReason;
 
-    public UUID getVehicleId() {
-        return vehicleId;
-    }
-
-    public void setVehicleId(UUID vehicleId) {
-        this.vehicleId = vehicleId;
-    }
-
-    public YearMonth getMonth() {
-        return month;
-    }
-
-    public void setMonth(YearMonth month) {
-        this.month = month;
-    }
-
-    public BigDecimal getInitialOdometer() {
-        return initialOdometer;
-    }
-
-    public void setInitialOdometer(BigDecimal initialOdometer) {
-        this.initialOdometer = initialOdometer;
-    }
-
-    public BigDecimal getFinalOdometer() {
-        return finalOdometer;
-    }
-
-    public void setFinalOdometer(BigDecimal finalOdometer) {
-        this.finalOdometer = finalOdometer;
-    }
-
-    public BigDecimal getProfessionalKilometers() {
-        return professionalKilometers;
-    }
-
+    public UUID getVehicleId() { return vehicleId; }
+    public void setVehicleId(UUID vehicleId) { this.vehicleId = vehicleId; }
+    public YearMonth getMonth() { return month; }
+    public void setMonth(YearMonth month) { this.month = month; }
+    public BigDecimal getInitialOdometer() { return initialOdometer; }
+    public void setInitialOdometer(BigDecimal initialOdometer) { this.initialOdometer = initialOdometer; }
+    public BigDecimal getFinalOdometer() { return finalOdometer; }
+    public void setFinalOdometer(BigDecimal finalOdometer) { this.finalOdometer = finalOdometer; }
+    public BigDecimal getProfessionalKilometers() { return professionalKilometers; }
     public void setProfessionalKilometers(BigDecimal professionalKilometers) {
         this.professionalKilometers = professionalKilometers;
     }
-
-    public String getAdjustmentReason() {
-        return adjustmentReason;
-    }
-
-    public void setAdjustmentReason(String adjustmentReason) {
-        this.adjustmentReason = adjustmentReason;
-    }
+    public boolean isManualAdjustment() { return manualAdjustment; }
+    public void setManualAdjustment(boolean manualAdjustment) { this.manualAdjustment = manualAdjustment; }
+    public boolean isConfirmWarnings() { return confirmWarnings; }
+    public void setConfirmWarnings(boolean confirmWarnings) { this.confirmWarnings = confirmWarnings; }
+    public String getAdjustmentReason() { return adjustmentReason; }
+    public void setAdjustmentReason(String adjustmentReason) { this.adjustmentReason = adjustmentReason; }
 }
